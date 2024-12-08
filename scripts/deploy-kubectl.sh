532f83de39892b0947b7bc6ba67b45eed17c0fa1
Eric <developer.ericgitangu@gmail.com>
2024-12-08 09:58:41 -0500

    Pull CRD config/

--- scripts/deploy-kubectl.sh
+++ scripts/deploy-kubectl.sh
@@ -144,7 +144,7 @@ fi
 
 # Istioctl install for CRDs
 echo "Installing Istio CRDs..."
-kubectl apply -f https://github.com/istio/istio/blob/1.24.1/manifests/charts/base/templates/crds.yaml
+kubectl apply -f https://github.com/istio/istio/blob/master/manifests/charts/base/files/crd-all.gen.yaml
 
 # Wait for CRDs to be established
 echo "Waiting for Istio CRDs to be established..."
